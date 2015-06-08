dd88b9827e31dfe48bcfdd2f13cac9781b163dd6
baardl <bard.lind@gmail.com>
2015-06-08 09:51:26 +0200

    Initial setup

---
+++ src/main/java/com/bardlind/blitz/App.java
@@ -0,0 +1,13 @@
+package com.bardlind.blitz;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
